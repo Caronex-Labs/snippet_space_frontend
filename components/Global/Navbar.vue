ecedf6726870f3ec1371bd740b2ba10324a73d97
SameeranB <sameeranbandishti@ieee.org>
2020-08-03 18:06:07 +0530

    Add desktop view

--- components/Global/Navbar.vue
+++ components/Global/Navbar.vue
@@ -18,15 +18,40 @@
         </v-img>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-app-bar-nav-icon dark @click="drawer = !drawer">
-
+      <v-app-bar-nav-icon v-if="mode === 'mobile'" dark @click="drawer = !drawer">
       </v-app-bar-nav-icon>
+      <template v-else>
+        <v-btn color="white" text exact @click="$vuetify.goTo('#banner')">
+          <v-icon color="primary" left>mdi-home</v-icon>
+          Home
+        </v-btn>
+        <v-divider class="mx-3" vertical inset color="white"></v-divider>
+        <v-btn color="white" text exact @click="$vuetify.goTo('#about-us')">
+          <v-icon color="accent" left>mdi-account-group</v-icon>
+          About Us
+        </v-btn>
+        <v-divider class="mx-3" vertical inset color="white"></v-divider>
+        <v-btn color="white" text exact @click="$vuetify.goTo('#contribute')">
+          <v-icon color="red" left>mdi-heart</v-icon>
+          Looking to Contribute?
+        </v-btn>
+        <v-divider class="mx-3" vertical inset color="white"></v-divider>
+        <v-btn color="white" text exact @click="oauthGithub">
+          <v-icon left color="white">mdi-github</v-icon>
+          Sign In With Github
+        </v-btn>
+        <v-divider class="mx-3" vertical inset color="white"></v-divider>
+        <v-btn @click="$vuetify.theme.dark = !$vuetify.theme.dark" icon>
+          <v-icon color="white">mdi-moon-waning-crescent</v-icon>
+        </v-btn>
+      </template>
     </v-app-bar>
 
     <v-navigation-drawer
       v-model="drawer"
       app
       bottom
+      right
       temporary
     >
       <v-list
@@ -56,7 +81,7 @@
           </v-list-item>
           <v-list-item @click="oauthGithub">
             <v-list-item-icon>
-              <v-icon color="black">mdi-github</v-icon>
+              <v-icon :color="$vuetify.theme.dark ? 'white' : 'black'">mdi-github</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Sign In With Github</v-list-item-title>
           </v-list-item>
@@ -89,7 +114,16 @@ export default {
   computed: {
     ...mapGetters({
       theme: 'landing/getTheme'
-    })
+    }),
+    mode() {
+      switch(this.$vuetify.breakpoint.name) {
+        case "xs": return 'mobile';
+        case "sm": return 'mobile';
+        case "md": return 'desk';
+        case "lg": return 'desk';
+        case "xl": return 'desk';
+      }
+    }
   },
   methods: {
     oauthGithub() {
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,11 @@
   <v-container class="ma-0 pa-0" fluid>
     <!--   Landing Page 1  -->
     <v-row
-      height="90vh"
       id="banner"
-      :style="$vuetify.theme.dark ? {backgroundColor: '#131B23'} : {backgroundColor: '#FFFBFE'}"    >
+      :style="$vuetify.theme.dark ? {backgroundColor: '#131B23', height: '92.5vh'} : {backgroundColor: '#FFFBFE', height: '92.5vh'}"    >
 
       <!--   Illustration   -->
-      <v-col class="mx-0 px-0" style="height: fit-content" cols="12">
+      <v-col v-if="mode === 'mobile'" class="mx-0 px-0" style="height: fit-content" cols="12">
         <v-img
           :src="$vuetify.theme.dark ? require('assets/Landing/mobile-banner-dark.svg') : require('assets/Landing/mobile-banner.svg')"
           width="100%"
@@ -16,14 +15,25 @@
         </v-img>
       </v-col>
 
+      <v-col v-else class="mx-0 px-0 d-flex align-end justify-center" cols="7">
+        <v-img
+          :src="$vuetify.theme.dark ? require('assets/Landing/desk-banner-dark.svg') : require('assets/Landing/desk-banner.svg')"
+          width="100%"
+          height="auto"
+        >
+        </v-img>
+      </v-col>
 
-      <v-col cols="12">
+
+
+
+      <v-col :cols="mode === 'mobile' ? 12 : 5" class="align-self-center">
 
         <!-- Quote Text  -->
 
         <v-row>
-          <v-col>
-            <p class="font-weight-bold font-italic text-center" style="font-size: 1.5rem; line-height: 2.438rem;">
+          <v-col >
+            <p class="font-weight-bold font-italic text-center" :style="$vuetify.breakpoint.name === 'xs' ? 'font-size: 1.5rem; line-height: 2.438rem' : 'font-size: 2.25rem; line-height: 2.6rem' ">
               <span class="accent--text">"</span>
               <span
                 class="secondary--text">Good programmers write code. <br>Great ones try not to.</span>
@@ -36,13 +46,13 @@
 
         <v-row>
           <v-col class="d-flex align-center justify-center">
-            <span class="secondary--text font-italic font-weight-light" style="font-size: 1.5rem; line-height: 2.25rem">And we're here to help.</span>
+            <span class="secondary--text font-italic font-weight-light" :style="$vuetify.breakpoint.name === 'xs' ? 'font-size: 1.5rem; line-height: 2.25rem' : 'font-size: 1.7rem; line-height: 2.25rem' ">And we're here to help.</span>
           </v-col>
         </v-row>
 
         <v-row class="my-6">
           <v-col class="d-flex flex-column align-center justify-center">
-            <v-btn dark class="primary--text">
+            <v-btn :x-large="$vuetify.breakpoint.name !== 'xs'" dark class="primary--text">
               Sign Up with Github
               <v-icon right>
                 mdi-github
@@ -76,22 +86,22 @@
     <v-row
       :style="$vuetify.theme.dark ? {backgroundColor: '#131B23', height: '93vh'} : {backgroundColor: '#FFFBFE', height: '93vh'}"
       id="about-us">
-      <v-col style="height: fit-content">
+      <v-col style="height: fit-content" :cols="mode === 'mobile' ? 12 : 5" class="d-flex align-center justify-center">
         <v-img
           :src="require('assets/Landing/circle-logo.svg')"
-          class="mt-8"
-          height="360"
-          width="381.05"
+          class="mx-auto mt-8"
+          height="100%"
+          width="auto"
         >
         </v-img>
       </v-col>
 
-      <v-col class="d-flex flex-column align-center mb-10">
-        <span class="secondary--text text-center mb-7" style="font-size: 1.5rem; line-height: 2.25rem">We Collect Your Best Codes.</span>
+      <v-col class="d-flex flex-column align-self-center mb-10">
+        <span class="secondary--text text-center mb-7" :style=" $vuetify.breakpoint.name === 'xs' ? 'font-size: 1.5rem; line-height: 2.25rem' : 'font-size: 2.2rem; line-height: 2.25rem'">We Collect Your Best Codes.</span>
         <v-spacer></v-spacer>
-        <span class="accent--text text-center" style="font-size: 1.2rem; line-height: 2.25rem">By Syncing With Your Github Gists Automatically.</span>
+        <span class="accent--text text-center" :style="$vuetify.breakpoint.name === 'xs' ? 'font-size: 1.2rem; line-height: 2.25rem' : 'font-size: 1.6rem; line-height: 2rem' ">By Syncing With Your Github Gists Automatically.</span>
         <v-spacer></v-spacer>
-        <span class="accent--text text-center" style="font-size: 1.2rem; line-height: 2.25rem">And Help You Organize, Tag and Manage Them.</span>
+        <span class="accent--text text-center" :style="$vuetify.breakpoint.name === 'xs' ? 'font-size: 1.2rem; line-height: 2.25rem' : 'font-size: 1.6rem; line-height: 2rem' ">And Help You Organize, Tag and Manage Them.</span>
         <v-spacer></v-spacer>
       </v-col>
     </v-row>
@@ -99,7 +109,7 @@
     <v-row
       :style="$vuetify.theme.dark ? {backgroundColor: '#131B23', height: '93vh'} : {backgroundColor: '#FFFBFE', height: '93vh'}"      id="contribute">
       <v-col class="d-flex flex-column justify-center align-center">
-        <span class="secondary--text text-center " style="font-size: 1.6rem; line-height: 2.25rem">This Project Is Completely Open Source.</span>
+        <span class="secondary--text text-center " :style="$vuetify.breakpoint.name === 'xs' ? 'font-size: 1.6rem; line-height: 2.25rem' : 'font-size: 2.4rem; line-height: 2.25rem' ">This Project Is Completely Open Source.</span>
         <v-avatar height="50vh" width="auto" tile>
           <v-img
             :src="$vuetify.theme.dark ? require('assets/Landing/open-source-dark.svg') : require('assets/Landing/open-source.svg')"
@@ -107,8 +117,8 @@
           </v-img>
         </v-avatar>
         <span class="secondary--text text-center font-weight-light mt-10"
-              style="font-size: 1.2rem; line-height: 2.25rem">Wish to contribute?</span>
-        <v-btn class="primary--text" outlined>
+              :style="$vuetify.breakpoint.name === 'xs' ? 'font-size: 1.2rem; line-height: 2.25rem' : 'font-size: 1.8rem; line-height: 2.25rem' ">Wish to contribute?</span>
+        <v-btn :x-large="$vuetify.breakpoint.name !== 'xs'" class="primary--text mt-5" outlined>
           Open In Github
           <v-icon right>
             mdi-github
@@ -130,7 +140,16 @@ export default {
   computed: {
     ...mapGetters({
       theme: 'landing/getTheme'
-    })
+    }),
+    mode() {
+      switch(this.$vuetify.breakpoint.name) {
+        case "xs": return 'mobile';
+        case "sm": return 'mobile';
+        case "md": return 'desk';
+        case "lg": return 'desk';
+        case "xl": return 'desk';
+      }
+    }
   }
 }
 </script>
