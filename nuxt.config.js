6fe206f7da31fea22ffad2d20648ece0bf0d6e63
SameeranB <sameeranbandishti@ieee.org>
2020-08-03 20:00:23 +0530

    add firebase config

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,11 +44,43 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/firebase'
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
+
+  firebase: {
+    config: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      measurementId: process.env.MEASUREMENT_ID
+    },
+    services: {
+      auth: {
+        ssr: true
+      }
+    }
+  },
+  pwa: {
+    meta: false,
+    icon: false,
+
+    workbox: {
+      importScripts: [
+        'firebase-auth-sw.js'
+      ],
+      dev: true
+    }
+  },
+
   axios: {
   },
   /*
